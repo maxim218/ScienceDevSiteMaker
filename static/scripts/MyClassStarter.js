b1c891609e5f2817275c2ead28334b311ada7b13
maxim <maxim218maxim218@yandex.ru>
2018-03-16 15:25:15 +0300

    Install old webpack

---
+++ static/scripts/MyClassStarter.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class MyClassStarter {
+    constructor() {
+        console.log("Create main obj MyClassStarter");
+    }
+}
+
+window.onload = function() {
+    const mainObj = new MyClassStarter();
+};
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+    entry: "./static/scripts/MyClassStarter.js",
+    output: {
+        filename: "./static/scripts/ANSWERSAITMAKER.js"
+    }
+};
